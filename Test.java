86209c099224d13af16f6bd3265665f896207c41
God <god@users.noreply.github.com>
2022-04-15 21:16:05 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ import java.io.RandomAccessFile;
 class Test extends Thread{
 	//\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ctc_ESurfing
 	static String readFor(RandomAccessFile pipe, String _for) throws Exception {
-		
+
 	    final ByteArrayOutputStream bao = new ByteArrayOutputStream();
 		    
 	    do{
@@ -15,6 +15,13 @@ class Test extends Thread{
 	        String current = bao.toString(StandardCharsets.UTF_16LE.name());
 	        
 	        if(current.endsWith("</M>")) {
+	        	
+	        	Pattern p = Pattern.compile("<[^<>//]*>[^<>]*</+[^<>]*>"); 
+	        	Matcher m = p.matcher(current); 
+	        	while(m.find()) { 
+	        	     System.out.println(m.group()); 
+	        	}
+	        	
 	        	System.out.println(current);
 	        	
 	        	if(!current.contains(_for))
@@ -35,16 +42,18 @@ class Test extends Thread{
 
 			  pipe.write(echoText.getBytes(StandardCharsets.UTF_16LE));
 			  
-			  System.out.println("write ok");
+			  System.out.println("============>\tClientStartComplete");
 			  
 			  System.out.println(readFor(pipe, "ClientStartComplete"));
 			  
 			  
-			  System.out.println("write ok2");
+			  System.out.println("============>\tCheckEnv");
 			  
 			  echoText = "<M><OpS>Portal</OpS><OpT>Portal</OpT><OpC>CheckEnv</OpC><P></P></M>";
 
 			  pipe.write(echoText.getBytes(StandardCharsets.UTF_16LE));
+			  
+			 
 			  
 			  System.out.println(readFor(pipe, "TicketResp"));
 			} catch (Exception e) {
