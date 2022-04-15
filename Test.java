4e4f907b5349187935b44dc4c632b4c55e49a6ee
God <god@users.noreply.github.com>
2022-04-15 19:38:29 +0800

    Add java port

---
+++ Test.java
@@ -0,0 +1,41 @@
+import java.io.ByteArrayOutputStream;
+import java.io.RandomAccessFile;
+
+class Test extends Thread{
+	
+	static String readFor(RandomAccessFile pipe, String _for) throws Exception {
+
+	    ByteArrayOutputStream bao = new ByteArrayOutputStream();
+		    
+	    do{
+	        int charCode = pipe.read();
+	        int charCodeLE = pipe.read();
+	        
+	          bao.write(charCode);
+	          bao.write(charCodeLE);
+	    } while(!bao.toString("utf-16le").contains(_for) || !bao.toString("utf-16le").endsWith("</M>"));
+	    
+	    return bao.toString("utf-16le").replace("</M>", "</M>\n");
+	}
+
+	
+	public static void main(String[] m) throws Exception {
+		try {
+			  RandomAccessFile pipe = new RandomAccessFile("\\\\.\\pipe\\ESurfingClientPipe", "rw");
+			  String echoText = "<M><OpS>Portal</OpS><OpT>Portal</OpT><OpC>ClientStart</OpC><P></P></M>";
+			  
+			  for(int i = 0 ; i < echoText.getBytes().length; i++) {
+				  pipe.write (echoText.getBytes()[i]);
+				  pipe.write ('\0');
+			  }
+			  
+			  pipe.getFD().sync();
+			  
+			  //It will blocks after close.
+			  
+			  System.out.println(readFor(pipe, "ClientStartComplete"));
+			} catch (Exception e) {
+			  e.printStackTrace();
+			}
+	}
+}
